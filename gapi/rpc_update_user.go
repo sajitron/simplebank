3fc3d6b619b8418f666ebf0290916bbc619398d1
Adams Banjo <sajioloye@gmail.com>
2022-10-13 23:15:56 -0400

    Add authorization to gRPC update

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -15,19 +15,19 @@ import (
 )
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
-	// authPayload, err := server.authorizeUser(ctx)
-	// if err != nil {
-	// 	return nil, unauthenticatedError(err)
-	// }
+	authPayload, err := server.authorizeUser(ctx)
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
 
 	violations := validateUpdateUserRequest(req)
 	if violations != nil {
 		return nil, invalidArgumentError(violations)
 	}
 
-	// if authPayload.Username != req.GetUsername() {
-	// 	return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
-	// }
+	if authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
+	}
 
 	arg := db.UpdateUserParams{
 		Username: req.GetUsername(),
